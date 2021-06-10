8b9692536730ea400e9a67958f075c7ed2e65831
aodin <aaronoellis@gmail.com>
2021-06-09 19:45:35 -0600

    Working with a TemplateView

---
+++ README.md
@@ -0,0 +1,4 @@
+Template Loader Bug
+====
+
+Testing the use of templates in DIRS from non-app code of a Django project
---
+++ template_loader_bug/template_loader_bug/views.py
@@ -0,0 +1,5 @@
+from django.views.generic import TemplateView
+
+
+class Example(TemplateView):
+    template_name = 'example.html'
